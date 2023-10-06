946e664466feb09baeb2d3572810953ef44668f0
a20eduardobn <a20eduardobn@iessanclemente.net>
2023-10-06 13:57:30 +0200

    Novos proxectos engadidos

---
+++ UD02_4LinearLayout/app/src/main/java/com/example/ud02_4linearlayout/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ud02_4linearlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.ud02_4linearlayout.R.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+        val buttonCallAction = findViewById<Button>(id.buttonCall)
+        val textLlamada = findViewById<TextView>(id.textCall)
+        buttonCallAction.setOnClickListener {
+            textLlamada.text=
+                getString(R.string.llamadaText)+findViewById<TextView>(id.nombreContacto).text.toString()
+        }
+    }
+}
