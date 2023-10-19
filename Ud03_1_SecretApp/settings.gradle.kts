5aa48d34446c6663ba31a585e5ffdf94bc37e891
a20eduardobn <a20eduardobn@iessanclemente.net>
2023-10-19 14:29:01 +0200

    Cambio de version

---
+++ Ud03_1_SecretApp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ud03_1_SecretApp"
+include(":app")
+ 
