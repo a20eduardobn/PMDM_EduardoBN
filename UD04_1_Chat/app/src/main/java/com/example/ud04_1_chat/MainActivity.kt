96a740ab001531bbda77b07d7d007ce847a6a7dd
a20eduardobn <a20eduardobn@iessanclemente.net>
2023-10-27 12:51:24 +0200

    Cambios no chat

--- UD04_1_Chat/app/src/main/java/com/example/ud04_1_chat/MainActivity.kt
+++ UD04_1_Chat/app/src/main/java/com/example/ud04_1_chat/MainActivity.kt
@@ -2,7 +2,17 @@ package com.example.ud04_1_chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavHostController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +20,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        //config barra superior
+        val navhostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navhostFragment.navController
+
+        val builderApp = AppBarConfiguration.Builder(navController.graph)
+        val configurationApp = builderApp.build()
+
+        toolbar.setupWithNavController(navController, configurationApp)
+
+        //config de la barra inferior
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setupWithNavController(navController)
+
+        //config barra lateral
+        val drawerlateral = findViewById<DrawerLayout>(R.id.drawer_layout)
+        builderApp.setOpenableLayout(drawerlateral)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
     }
 }
