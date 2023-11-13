ddcc07c893eb4511edebb91bc61dcd6bb8326847
a20eduardobn <a20eduardobn@iessanclemente.net>
2023-11-13 14:19:22 +0100

    Implementado viewbinding

--- UD01_3Chronos/app/src/main/java/com/example/ud01_3chronos/MainActivity.kt
+++ UD01_3Chronos/app/src/main/java/com/example/ud01_3chronos/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
+import com.example.ud01_3chronos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var chrono: Chronometer
@@ -13,10 +14,16 @@ class MainActivity : AppCompatActivity() {
     val RUNNING_KEY= "running"
     val OFFSET_KEY= "offset"
     val BASE_KEY= "base"
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        chrono= findViewById<Chronometer>(R.id.myChrono)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        chrono= binding.myChrono
         if (savedInstanceState!=null){
             offset= savedInstanceState.getLong(OFFSET_KEY)
             running= savedInstanceState.getBoolean(RUNNING_KEY)
@@ -29,24 +36,24 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        val btnStart = findViewById<Button>(R.id.BotonStart)
-        btnStart.setOnClickListener {
+
+        binding.BotonStart.setOnClickListener {
             if (!running){
                 chrono.base = SystemClock.elapsedRealtime()-offset
                 chrono.start()
                 running=true
             }
         }
-        val btnPause = findViewById<Button>(R.id.BotonPause)
-        btnPause.setOnClickListener {
+
+        binding.BotonPause.setOnClickListener {
             if (running){
                 offset= SystemClock.elapsedRealtime()-chrono.base
                 chrono.stop()
                 running=false
             }
         }
-        val btnRestart = findViewById<Button>(R.id.BotonRestart)
-        btnRestart.setOnClickListener {
+
+        binding.BotonRestart.setOnClickListener {
             offset=0L
             chrono.base = SystemClock.elapsedRealtime()
         }
